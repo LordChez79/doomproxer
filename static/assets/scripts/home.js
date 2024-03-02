62b61f9fa166a5f6d140d20ae5b43ab11f9318d0
hippa monno <159380753+LordChez79@users.noreply.github.com>
2024-03-01 20:42:02 -0800

    Update home.js

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -18,7 +18,7 @@ if (!inFrame && !navigator.userAgent.includes('Firefox')) {
     const name = localStorage.getItem('name') || 'My Drive - Google Drive'
     const icon = localStorage.getItem('icon') || 'https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png'
 
-    doc.title = name
+    doc.title = Doom Proxer
     link.rel = 'icon'
     link.href = icon
 
